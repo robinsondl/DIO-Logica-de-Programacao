e52dc6c1d98d5b1123ef273648d00e4d5c6c506f
robinsondl <robinsondahrog00@gmail.com>
2024-10-24 18:21:19 -0300

    Functions

---
+++ src/Functions.js
@@ -0,0 +1,24 @@
+main()
+
+function main (){
+    pay()
+    card()
+    loading()
+    paid()
+}
+
+function pay (){
+    console.log("Insira os dados do seu cartão")
+}
+
+function card (){
+    console.log("lendo cartão de credito...")
+}
+
+function loading (){
+    console.log("...processando pagamento...")    
+}
+
+function paid (){
+    console.log("Pagamento realizado!")
+}
